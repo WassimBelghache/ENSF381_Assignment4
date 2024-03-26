53f13b8bfd641ddaff764bc11e3d736ab2e61513
WassimBelghache <wassim.belghache@ucalgary.ca>
2024-03-25 22:07:58 -0600

    app.js changes

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Homepage from './component/Homepage';
+import ProductPage from './component/ProductPage';
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentPage === 'home' && <Homepage navigateTo={navigateTo} />}
+      {currentPage === 'product' && <ProductPage navigateTo={navigateTo} />}
     </div>
   );
 }
