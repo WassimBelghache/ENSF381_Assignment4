4d77964dd06e533f7ca2ed57cb578dcc55c31584
WassimBelghache <wassim.belghache@ucalgary.ca>
2024-04-05 21:56:13 -0600

    added missing code

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path = '/' element={<Homepage />} />
-
+      <Route path = '/Login' element={<LoginPage />} />
       <Route path = '/Products' element={<Productpage />} />
     </Routes>
     </BrowserRouter>
